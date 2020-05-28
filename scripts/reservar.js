97c60372336954d30ebf63b58aae2a9bb7ed1576
AURA <aura@MacBook-Pro-de-AURA.local>
2020-05-27 23:57:56 -0500

    Adding google maps API, script folder and styles for reservaciones view, not completed yet

--- scripts/reservar.js
+++ scripts/reservar.js
@@ -1,1 +1,19 @@
+function initMap() {
+  var coord = {
+    lat: 20.6739383, 
+    lng: -103.4054541};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
 
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  })
+
+}
+
+document.addEventListener('.burger')('click', ()=>{
+  document.querySelector('.burger').classList.add("isVisible");
+})
